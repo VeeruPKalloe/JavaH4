4df1e938e394f416e22a8cfc5883031ef4a47489
VeeruPKalloe <2068003@talnet.n>
2017-09-11 10:14:40 +0200

    Praktijk opdracht hoofdst.4

---
+++ src/H4PraktijkOpdracht.java
@@ -0,0 +1,38 @@
+//Voorbeeld 4.1
+
+import java.awt.*;
+import java.applet.*;
+
+public class H4PraktijkOpdracht extends Applet {
+
+    public void init() {
+    }
+
+    public void paint(Graphics g) {
+        g.drawLine(50, 50, 175, 50);
+        g.drawString("lijn", 110, 65);
+        g.drawRect(50,150, 250, 150);
+        g.drawString("rechthoek", 135, 320);
+        g.setColor(Color.pink);
+        g.fillRect(325, 150, 250, 150);
+        g.setColor(Color.BLACK);
+        g.drawOval(325, 150, 250, 150);
+        g.drawString("Gevulde rechthoek", 400, 325);
+        g.drawOval(600, 150, 250, 150);
+        g.fillArc(620, 150, 200,123, 45, 90);
+        g.drawRoundRect(50, 350, 250, 150, 20, 20);
+        g.drawString("Afgeronde rechthoek", 100, 525);
+        g.setColor(Color.pink);
+        g.fillOval(325, 350, 250, 150);
+        g.setColor(Color.BLACK);
+        g.drawOval(650, 350, 175, 175);
+        g.drawString("gevulde ovaal", 400, 525);
+        g.drawString("cirkel", 725, 540);
+
+
+
+
+
+
+    }
+}
